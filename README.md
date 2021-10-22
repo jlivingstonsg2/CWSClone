4dd5a80fe31a2c7573169dec592f8c76c2bdbfd6
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-10-22 09:34:38 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# WebPage8
+
+Close VS 2019 and delete the two folders ".vs" and "bin" to get a working project in VS 2019.
+
+Good luck  :)
+
+
+Regards 
---
+++ WebPage8/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPage8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BrandItems()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult Items()
+        {
+            return View();
+        }
+       
+    }
+}
